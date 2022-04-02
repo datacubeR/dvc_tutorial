6d72e30554ee981e9cfa93403c18b2e686ed86b9
Alfonso Tobar <big_drummer2@hotmail.com>
2022-04-02 18:34:11 -0300

    Interfaz de Experimentacion v3

--- exp_file.sh
+++ exp_file.sh
@@ -1,7 +1,7 @@
-dvc exp run --queue -S train.C=0.01
-dvc exp run --queue -S train.C=1
-dvc exp run --queue -S train.C=20
-dvc exp run --queue -S train.C=50
-dvc exp run --queue -S train.C=100
+dvc exp run --queue -S train.C=0.001
+dvc exp run --queue -S train.C=5
+dvc exp run --queue -S train.C=30
+dvc exp run --queue -S train.C=60
+dvc exp run --queue -S train.C=120
 
 dvc exp run --run-all
